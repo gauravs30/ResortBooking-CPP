27146e4c8a66c0711f24422c5b42041f494e1001
gauravs30 <gaurav.chinchane25@gmail.com>
2023-04-22 12:54:06 +0100

    Initial commit

---
+++ hideaway/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Resorts, Rooms, Reservation
+
+# Register your models here.
+admin.site.register(Resorts)
+admin.site.register(Rooms)
+admin.site.register(Reservation)
